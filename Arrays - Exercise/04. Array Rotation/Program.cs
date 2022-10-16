70700a7de862edda9d4558f924df9f7757364013
DanAleksiev <toksikar46@gmail.com>
2022-10-16 19:19:11 +0100

    update

--- Arrays - Exercise/04. Array Rotation/Program.cs
+++ Arrays - Exercise/04. Array Rotation/Program.cs
@@ -6,18 +6,18 @@ namespace _04._Array_Rotation
         {
         static void Main(string[] args)
             {
-            string[] input=Console.ReadLine()
+            string[] input = Console.ReadLine()
                 .Split(' ');
             int positions = int.Parse(Console.ReadLine());
-            int numOfRotations = positions%input.Length;
-            for(int i = 0; i < numOfRotations; i++)
+            int numOfRotations = positions % input.Length;
+            for (int i = 0; i < numOfRotations; i++)
                 {
                 string savedNum = input[0];
-                for(int j = 1; j < input.Length; j++)
+                for (int j = 1; j < input.Length; j++)
                     {
-                    input[j-1] = input[j];
+                    input[j - 1] = input[j];
                     }
-                input[input.Length-1] = savedNum;
+                input[input.Length - 1] = savedNum;
                 }
             Console.WriteLine(string.Join(" ", input));
             }
