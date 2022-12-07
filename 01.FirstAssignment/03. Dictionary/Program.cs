75d8fad514e94f82a3f09468554e72a5ae60f9a4
DanAleksiev <toksikar46@gmail.com>
2022-12-07 08:56:28 +0000

    update

---
+++ 01.FirstAssignment/03. Dictionary/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _03._Dictionary
+    {
+    internal class Program
+        {
+        static void Main(string[] args)
+            {
+            string input = Console.ReadLine();
+            List<string> splitInput = input.Split(" | ").ToList();
+
+            Dictionary<string, string> words = new Dictionary<string, string>();
+            foreach (string word in splitInput)
+                {
+                List<string> list = word.Split(": ").ToList();
+                string currentWord = list[0];
+                string currentDeff = list[1];
+                if (words.ContainsKey(currentWord))
+                    {
+                    string oldDEf = words[currentWord];
+                    words[currentWord] = $"{oldDEf}|{currentDeff}";
+                    }
+                else
+                    {
+                    words[currentWord] = currentDeff;
+                    }
+                }
+            List<string> wordToBeTested = new List<string>();
+            input = Console.ReadLine();
+            while (input != "Test" && input != "Hand Over")
+                {
+                List<string> splitWords = input.Split(" | ").ToList();
+                foreach (string word in splitWords)
+                    {
+                    wordToBeTested.Add(word);
+                    }
+                input = Console.ReadLine();
+                }
+
+            if (input == "Test")
+                {
+                foreach (string word in wordToBeTested)
+                    {
+                    if (words.ContainsKey(word))
+                        {
+                        string wordDeff = words[word];
+                        List<string> splitValue = wordDeff.Split("|").ToList();
+                        Console.WriteLine($"{word}:");
+                        foreach (var deff in splitValue)
+                            {
+                            Console.WriteLine($"-{deff}");
+                            }
+                        }
+                    }
+                }
+            if (input == "Hand Over")
+                {
+                foreach (var word in words)
+                    {
+                    Console.Write($"{word.Key} ");
+                    }
+                }
+            }
+        }
+    }
---
+++ 01.FirstAssignment/FinalExamFundamentals/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinalExamFundamentals
+    {
+    internal class Program
+        {
+        static void Main(string[] args)
+            {
+            string originalString = Console.ReadLine();
+            StringBuilder decript = new StringBuilder(originalString);
+
+            string input;
+            while ((input = Console.ReadLine()) != "Finish")
+                {
+                List<string> splitInput = input.Split(' ').ToList();
+                string action = splitInput[0];
+                if (action == "Replace")
+                    {
+                    string target = splitInput[1];
+                    string replacement = splitInput[2];
+                    decript.Replace(target, replacement);
+                    Console.WriteLine(decript);
+                    }
+                else if (action == "Cut")
+                    {
+                    int startIndex = int.Parse(splitInput[1]);
+                    int endIndex = int.Parse(splitInput[2]);
+                    int lenght = endIndex - startIndex;
+                    if (startIndex < decript.Length && endIndex < decript.Length && startIndex >= 0 && endIndex >= 0)
+                        {
+                        decript.Remove(startIndex, lenght + 1);
+                        Console.WriteLine(decript);
+                        }
+                    else
+                        {
+                        Console.WriteLine("Invalid indices!");
+                        }
+                    }
+                else if (action == "Make")
+                    {
+                    string upOrLow = splitInput[1];
+                    StringBuilder temp = new StringBuilder();
+                    if (upOrLow == "Upper")
+                        {
+                        temp.Append(decript.ToString().ToUpper());
+                        decript.Clear();
+                        decript.Append(temp);
+                        }
+                    else if (upOrLow == "Lower")
+                        {
+                        temp.Append(decript.ToString().ToLower());
+                        decript.Clear();
+                        decript.Append(temp);
+                        }
+                    temp.Clear();
+                    Console.WriteLine(decript);
+                    }
+                else if (action == "Check")
+                    {
+                    string isContain = splitInput[1];
+                    if (decript.ToString().Contains(isContain))
+                        {
+                        Console.WriteLine($"Message contains {isContain}");
+                        }
+                    else
+                        {
+                        Console.WriteLine($"Message doesn't contain {isContain}");
+                        }
+                    }
+                else if (action == "Sum")
+                    {
+                    int startIndex = int.Parse(splitInput[1]);
+                    int endIndex = int.Parse(splitInput[2]);
+                    int sum = 0;
+                    if (startIndex < decript.Length && endIndex < decript.Length && startIndex >= 0 && endIndex >= 0)
+                        {
+                        for (int i = startIndex; i <= endIndex; i++)
+                            {
+                            int charValue = decript[i];
+                            sum += charValue;
+                            }
+                        Console.WriteLine(sum);
+                        }
+                    else
+                        {
+                        Console.WriteLine("Invalid indices!");
+                        }
+                    }
+                }
+            }
+        }
+    }
